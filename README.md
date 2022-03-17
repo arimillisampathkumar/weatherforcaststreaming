f0c571c7b8ab4b153f6cf081f445ec8c0c65fa88
Sampath Arimilli <arimillisampathkumar@gmail.com>
2022-03-17 18:22:41 +0800

    update task3 results

--- README.md
+++ README.md
@@ -2,24 +2,51 @@
 Weather forecast Realtime Streaming Application
 
 Requirement:
+============
 	Obtain realtime weather data from the API from openweathermap.org website
 	Create a realtime streaming application that ingests the input data from the source.
 	Generate report as business requirement
 
 High Level Solution:
+====================
 	This application has been divided in to two parts
 	1.)	RAW data ingestion as csv
 	2.) Spark application to stream the data and transform as per requirement
 
 HighLevel Design:
-![alt text](Design.png)
+=================
 
-Checkout the Project and Build using Maven:
---mvn clean package
 
-As part of the solution we have designed this solution to write the RAW data to local directories called "raw" and "input"(Input folder location I have used for development). But our program has been designed read from any desired location specified as program argument
+![alt text](DesignDoc.png)
 
-Command to execute the Spark Application after build:
---spark-submit --master <yarn or "local[*]"> --deploy-mode <deploy_mode_if_yarn> /<absolute_path_to_jar>/SparkStreaming-0.0.1-SNAPSHOT.jar <config_file_absolute_path> <input_file_path_for_streaming>
- 
+
+Solution:
+===========
+a.)	I have designed this solution to write the RAW data to local directories called "raw"(Json) and "input"(csv). 
+b.)	A deamon thread would run in the background along with streaming application which would extract data from API and write raw and input folders. Note:- we will this input folder for spark streaming
+c.) our streaming application would read data from the above input folder and write the results to console
+
+Steps to Run the application:
+=============================
+1.)	Checkout the Project and Build using Maven:
+	
+	mvn clean package
+
+2.)	create two directories in the current folder using following commands:
+
+	mkdir input
+	mkdir raw
+	
+3.) Create config file(config.prop) in current working directory with following content:
+	
+	locations=HongKong,Singapore,Tokyo,Seoul,London,Paris,NewYork
+	app_key=86360c8475357ebb01df5334aa34a6ed
+	frequency_sec=15
+	aggregate_period_sec=60
+
+4.)	Command to execute the Spark Application after build:
+	
+	spark-submit --master <yarn or "local[*]"> --deploy-mode <deploy_mode_if_yarn> /<absolute_path_to_jar>/SparkStreaming-0.0.1-SNAPSHOT.jar <config_file_absolute_path> <input_folder_path_created_above>
+
+
 
--- src/main/java/edu/stream/spark/ProcessData.java
+++ src/main/java/edu/stream/spark/ProcessData.java
@@ -5,17 +5,24 @@ import static org.apache.spark.sql.functions.col;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.concurrent.TimeoutException;
 
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.IntegerType;
 import org.apache.spark.sql.types.StructType;
+
+import scala.Function1;
+import scala.reflect.ClassTag;
 
 public class ProcessData {
 	public static void main(String args[]) throws StreamingQueryException, TimeoutException {
@@ -41,7 +48,7 @@ public class ProcessData {
 				.add("wind_deg", "decimal").add("clouds_all", "decimal").add("dt", "long").add("sys_id", "integer")
 				.add("sys_type", "integer").add("country", "string").add("sunrise", "long").add("sunset", "long")
 				.add("id", "long").add("name", "string").add("cod", "string");
-		SparkSession spark = SparkSession.builder().appName("SparkKafkaStreamingJob").getOrCreate();
+		SparkSession spark = SparkSession.builder().master("local[*]").appName("SparkKafkaStreamingJob").getOrCreate();
 		spark.udf().register("getFrequentDes", new UDF1<String, String>() {
 			  @Override
 			  public String call(String wordsStr) {
@@ -82,7 +89,6 @@ public class ProcessData {
 				.select(col("name").alias("location"),col("window.start").alias("datetime"),col
 						("avg_temperature"), col("_max"), col("_min"),functions.call_udf("getFrequentDes", col("freq_desc")));
 		
-
 	
 
 		
@@ -91,20 +97,31 @@ public class ProcessData {
 		  col("_max").minus(col("_min")).alias("temperature_diff"),col("getFrequentDes(freq_desc)").alias("desc"));
 		  streamingTaskResult_1.writeStream().outputMode("complete").format("console").start();
 		 
+		  	
 
-
-
-		
-			
 			/*
-			 * Dataset<Row> dataSet3=streamingTaskResult_1.groupBy(col("datetime"))
-			 * .agg(functions.avg("avg_temperature").alias("temp"),
-			 * functions.max("avg_temperature").minus(functions.min("avg_temperature").alias
-			 * ("_min")).alias( "temperature_change"));
+			 * Dataset<Row>
+			 * dataSet3=streamingTaskResult_1.withColumn("avg_global_temperature",
+			 * functions.avg("avg_temperature").over(
+			 * Window.partitionBy(col("datetime"))).alias("temp"))
+			 * .withColumn("temperature_change",
+			 * functions.col("avg_temperature").minus(functions.lag(col("avg_temperature"),
+			 * 120).over( Window.partitionBy(col("datetime"))).alias("temp")));
+			 * 
 			 * dataSet3.writeStream().outputMode("complete").format("console").start();
 			 */
 			 
-
+			/*
+			 * org.apache.spark.sql.AnalysisException: Non-time-based windows are not
+			 * supported on streaming DataFrames/Datasets; Window [avg(avg_temperature#62)
+			 * windowspecdefinition(datetime#78, specifiedwindowframe(RowFrame,
+			 * unboundedpreceding$(), unboundedfollowing$())) AS temp#95], [datetime#78] +-
+			 * Project [location#77, datetime#78, avg_temperature#62, temperature_diff#87,
+			 * desc#88] +- Project [location#77, datetime#78, avg_temperature#62,
+			 * CheckOverflow((promote_precision(cast(_max#64 as decimal(11,0))) -
+			 * promote_precision(cast(_min#66 as decimal(11,0)))), DecimalType(11,0), true)
+			 * AS temperature_diff#87, getFrequentDes(freq_desc)#80 AS desc#88]
+			 */
 	}
 
 
